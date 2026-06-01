fc923980d179641e047eb6ec2a2eb0dcde41771e
Miljana0609 <miljanazoranovic25@gmail.com>
2026-06-01 20:10:30 +0200

    Add CorsConfig and change url

--- src/main/java/se/jensen/miljana/fakestoreservice/service/ProductService.java
+++ src/main/java/se/jensen/miljana/fakestoreservice/service/ProductService.java
@@ -23,7 +23,7 @@ public class ProductService {
 
     public List<Product> fetchAndSaveProducts() {
 
-        String url = "http://fakestoreservice-env.eba-qva25xqa.eu-north-1.elasticbeanstalk.com/products";
+        String url = "http://fakestoreservice-env.eba-pmbni5kh.eu-north-1.elasticbeanstalk.com/";
 
         Product[] response = restTemplate.getForObject(
                 url,
